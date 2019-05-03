de90c5244f9c1006d979cb28040aeb34b5de9f65
Zita Nguyen <zitanguyen84@gmail.com>
2019-05-03 12:37:59 +0200

    connexion
    modify use ...Annotations, still struggle

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -7,7 +7,7 @@ namespace App\Controller;
 use App\Entity\Membre;
 use App\Form\ConnexionFormType;
 use App\Form\MembreFormType;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
