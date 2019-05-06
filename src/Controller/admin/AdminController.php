3f5a65e2b8d7a137587ca9424b88a046d5a6eef1
saadatou <zinarya@yahoo.fr>
2019-05-06 10:57:29 +0200

    Revert "Revert "Admin/ login""
    This reverts commit ebb9d15e636917b725eaa26b91be34305894a862.

--- src/Controller/admin/AdminController.php
+++ src/Controller/admin/AdminController.php
@@ -9,11 +9,63 @@
 namespace App\Controller\admin;
 
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
+use Symfony\Component\Routing\Annotation\Route;
 
-class AdminController extends AbstractController
+class AdminController extends EasyAdminController
 {
+    // ...
 
+    /*public function updateEntity($entity)
+    {
+        if (method_exists($entity, 'setUpdatedAt')) {
+            $entity->setUpdatedAt(new \DateTime());
+        }
 
+        parent::updateEntity($entity);
+    }*/
+
+    public function persistEntity($entity)
+    {
+        $this->updateSlug($entity);
+        parent::persistEntity($entity);
+    }
+
+    public function updateEntity($entity)
+    {
+        $this->updateSlug($entity);
+        parent::updateEntity($entity);
+    }
+
+    private function updateSlug($entity)
+    {
+        if (method_exists($entity, 'setSlug') and method_exists($entity, 'getTitle')) {
+            $entity->setSlug($this->slugger->slugify($entity->getTitle()));
+        }
+    }
+
+    // Customizes the instantiation of entities only for the 'User' entity
+    public function createNewUserEntity()
+    {
+        return new User(array('ROLE_USER'));
+    }
+
+    /*******************************************************************/
+    /**
+     * @Route("/login" , name="login")
+     */
+    public function loginAction()
+    {
+        return $this->render('admin/login.html.twig');
+    }
+
+    /**
+     * @Route("/logout" )
+     * @throws \RuntimeException
+     */
+    public function logoutAction()
+    {
+        throw new \RuntimeException('Ce message ne doit pas etre atteint');
+    }
 
 }
