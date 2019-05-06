7a38537a5f3ad281e04e4a0d77aea42967b4f95b
Zinaria <saadatou.alidankintafo@gmail.com>
2019-05-06 08:48:57 +0200

    Admin/ login
    Admin/ login

---
+++ src/Controller/admin/ArticleOfficeController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Karim
+ * Date: 05/05/2019
+ * Time: 18:21
+ */
+
+namespace App\Controller\admin;
+
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
+
+class ArticleOfficeController extends EasyAdminController
+{
+
+}
--- src/Migrations/Version20190505144437.php
+++ src/Migrations/Version20190505144437.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190503081427 extends AbstractMigration
+final class Version20190505144437 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,8 @@ final class Version20190503081427 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE article CHANGE photo photo VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE article ADD photo VARCHAR(255) NOT NULL, ADD poids DOUBLE PRECISION NOT NULL, ADD date_peremption DATETIME NOT NULL, ADD ingredients LONGTEXT NOT NULL, DROP photos');
+        $this->addSql('ALTER TABLE membre CHANGE roles role LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\'');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +31,7 @@ final class Version20190503081427 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE article CHANGE photo photo LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci COMMENT \'(DC2Type:array)\'');
+        $this->addSql('ALTER TABLE article ADD photos LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci COMMENT \'(DC2Type:array)\', DROP photo, DROP poids, DROP date_peremption, DROP ingredients');
+        $this->addSql('ALTER TABLE membre CHANGE role roles LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci COMMENT \'(DC2Type:array)\'');
     }
 }
